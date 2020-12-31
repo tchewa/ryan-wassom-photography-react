9a2cb898d31014863437b938893a1c73de2b1c33
Ryan Wassom <ryan.wassom@clearlink.com>
2020-12-31 11:20:34 -0700

    updated package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
-import * as React from "react"
+import * as React from 'react'
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 // markup
 const IndexPage = () => {
   return (
-    <p>this is the home page</p>
+    <Container>
+      <p>this is the home page</p>
+    </Container>
   );
 }
 
